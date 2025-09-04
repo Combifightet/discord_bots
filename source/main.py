ebddc906ecdb4417b8674fbd3dff14556f2f0a51
Combifightet <malte@hei-sharp-s.de>
2025-09-04 10:44:10 +0200

    [Feat] added axample cog and auto loading

--- source/main.py
+++ source/main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
+# from cogs import embed
+
 BOT_TOKEN:str = os.getenv('BOT_TOKEN')
 
 intents = discord.Intents.default()
@@ -13,6 +15,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
 	print(f"Logged in as {bot.user}")
 
+	for filename in os.listdir('source/cogs'):
+		if filename.endswith('.py'):
+			await bot.load_extension(f'cogs.{filename[:-3]}')
+
+
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def ping(ctx):
@@ -23,6 +30,9 @@ async def ping(ctx):
 
 def main():
 	bot.run(BOT_TOKEN)
+	
+
+
 
 if __name__ == '__main__':
 	main()
